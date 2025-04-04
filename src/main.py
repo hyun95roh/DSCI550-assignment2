b1dfb03c7bfcfb75e95e4cec83c474a841a48866
hyun95roh <156367419+hyun95roh@users.noreply.github.com>
2025-04-04 05:30:10 -0700

    requirements.txt update

--- src/main.py
+++ src/main.py
@@ -1,2 +1,5 @@
-# For assignment2, the main.py has nothing to do to produce tsv2. 
+# For assignment2, the main.py is just placeholder and has nothing to do with producing tsv2. 
 # All works(AI image generations, GeoTopicParsing, Spacy) are done by the notebook files seperately. 
+
+msg = 'Refer to notebooks. For assignment2, the main.py is just placeholder and has nothing to do with producing tsv2. All works(AI image generations, GeoTopicParsing, Spacy) are done by the notebook files seperately. '
+print(msg)
